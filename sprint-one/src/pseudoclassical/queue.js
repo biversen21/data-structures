b5c23b36fbf33a0db00dc4314cba1c975620f77f
biversen21 <biversen21@gmail.com>
2014-09-11 10:18:41 -0700

    passed pseudoclassical queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.hiddenSize = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.hiddenSize] = value;
+  this.hiddenSize++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.hiddenSize > 0) {
+    var temp = this.storage[0];
+    delete this.storage[0];
+    for (var key in this.storage) {
+      this.storage[key - 1] = this.storage[key];
+    }
+    this.hiddenSize--;
+    return temp;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.hiddenSize;
 };
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.hiddenSize = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.hiddenSize] = value;
+  this.hiddenSize++;
+}
 
+Stack.prototype.pop = function() {
+  if(this.hiddenSize > 0) {
+    var popped = this.storage[this.hiddenSize - 1];
+    delete this.storage[this.hiddenSize - 1];
+    this.hiddenSize--;
+    return popped;
+  }
+}
+
+Stack.prototype.size = function() {
+  return this.hiddenSize;
+}
