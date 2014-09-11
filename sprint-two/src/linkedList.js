376794598ba42edb2f65ae9c7326978377013836
biversen21 <biversen21@gmail.com>
2014-09-11 15:52:46 -0700

    Basic reqs for linked list and set

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    list[node.value] = node;
+
+    if (list.head === null) {
+      list.head = node;
+      list.tail = node;
+    } else  {
+      list.tail.next = node;
+      list.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var removed = list.head;
+    list.head = list.head.next;
+    delete list[removed.value];
+    return removed.value;
   };
 
   list.contains = function(target){
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    } // else
+    return false;
   };
 
   return list;
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,20 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if (this._storage === undefined) {
+    this._storage = {};
+  }
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._storage[item] !== undefined ? true : false;
 };
 
 setPrototype.remove = function(item){
+  var removed = this._storage[item];
+  delete this._storage[item];
+  return removed;
 };
 
 /*
