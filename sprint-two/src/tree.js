98db8f5d98885dd409679d30b570f7fa872b058e
biversen21 <biversen21@gmail.com>
2014-09-11 20:13:27 -0700

    implimented tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree,treeMethods);
   return newTree;
 };
 
@@ -11,11 +12,29 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var tree = makeTree(value);
 
+  if (this.children === undefined) {
+    this.children = [];
+  }
+  this.children[this.children.length] = tree;
 };
 
 treeMethods.contains = function(target){
-
+  var result = false;
+  var recurse = function(node) {
+    if (node.value === target) {
+      result = true;
+    } else {
+      if (node.children) {
+        for (var i = 0; i < node.children.length; i++) {
+          recurse(node.children[i]);
+        }
+      }
+    }
+  }
+  recurse(this);
+  return result;
 };
 
 
