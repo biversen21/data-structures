d594b52cd7e5a97250cc881a53cf3910c15db76d
biversen21 <biversen21@gmail.com>
2014-09-11 21:20:15 -0700

    passed 2/3 methods for binarySearchTree
    passed insert() and contains() methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,62 @@
 var makeBinarySearchTree = function(value){
+  var newTree = Object.create(binaryTreeMethods);
+  newTree.children = undefined;
+  newTree.left = undefined;
+  newTree.right = undefined;
+  newTree.value = value;
+  return newTree;
 };
 
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+
+  var recurse = function(node) {
+    if (value >= node.value) {
+      if (node.right) {
+        recurse(node.right)
+      } else {
+        node.right = makeBinarySearchTree(value);
+      }
+    } else {
+      if (node.left) {
+        recurse(node.left)
+      } else {
+        node.left = makeBinarySearchTree(value);
+      }
+    }
+  }
+  recurse(this);
+};
+
+binaryTreeMethods.contains = function (target) {
+  var result = false;
+  var nextNode;
+  var recurse = function(node) {
+    if (node !== undefined) {
+
+      if (node.value === target) {
+        result = true;
+        nextNode = undefined;
+      } else {
+        if (target > node.value) {
+          nextNode = node.right;
+        } else {
+          nextNode = node.left;
+        }
+      }
+    }
+    if (nextNode) {
+      recurse(nextNode);
+    }
+  }
+  recurse(this);
+  return result;
+};
+
+binaryTreeMethods.depthFirstLog = function (callback) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
