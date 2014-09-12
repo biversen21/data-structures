142f5110051a343c60f8c9fb6eee4053e70ea29f
biversen21 <biversen21@gmail.com>
2014-09-12 15:43:30 -0700

    passed all hashtable basic tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,36 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var arr = this._storage.get(i);
+  if (arr === undefined) {
+    var value = [];
+    value.push({k: k, v: v});
+    this._storage.set(i, value);
+  } else {
+    arr.push({k:k, v:v});
+    this._storage.set(i, arr);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var arr = this._storage.get(i);
+  for (var j = 0; j < arr.length; j++) {
+    if (arr[j].k === k) {
+      return arr[j].v;
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var arr = this._storage.get(i);
+  for (var j = 0; j < arr.length; j++) {
+    if (arr[j].k === k) {
+      arr[j] = {k: k, v: null};
+    }
+  }
+  this._storage.set(i, arr);
 };
 
 
