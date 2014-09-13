d5be900c566e93d11aac97cad7e5cbdc6c07d949
biversen21 <biversen21@gmail.com>
2014-09-12 21:30:18 -0700

    hash table doubles successfully

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -1,10 +1,10 @@
 describe('hashTable', function() {
   var hashTable;
-  var people = [['Steven', 'Tyler'], ['George', 'Harrison'], ['Mr.', 'Doob'], ['Dr.', 'Sunshine'], ['John', 'Resig'], ['Brendan', 'Eich'], ['Alan', 'Turing']];
+  var people = [['Steven', 'Tyler'],['James','Ferguson'], ['George', 'Harrison'], ['Mr.', 'Doob'], ['Dr.', 'Sunshine'], ['John', 'Resig'], ['Brendan', 'Eich'], ['Alan', 'Turing']];
 
 
   beforeEach(function() {
-    hashTable = new HashTable();
+    hashTable = new HashTable(8);
   });
 
   it('should have methods named "insert", "remove", and "retrieve', function() {
@@ -42,7 +42,7 @@ describe('hashTable', function() {
   });
 
   // (Extra credit! Remove the extra "x" when you want the following tests to run)
-  xit('should double in size when needed', function() {
+  it('should double in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0], lastName = person[1];
       hashTable.insert(firstName,lastName);
@@ -50,7 +50,7 @@ describe('hashTable', function() {
     expect(hashTable._limit).to.equal(16);
   });
 
-  xit('should halve in size when needed', function() {
+  it('should halve in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0], lastName = person[1];
       hashTable.insert(firstName,lastName);
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,19 +1,48 @@
-var HashTable = function(){
-  this._limit = 8;
+var HashTable = function(limit){
+  this._limit = limit;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
 };
+
+HashTable.prototype.resize = function() {
+  console.log(this._size);
+  console.log(this._limit);
+  if (this._size / this._limit >= 0.75) {
+    var tempArr = [];
+    this._storage.each(function(v, k, c) {
+      tempArr.push(v);
+    });
+    // console.log(tempArr);
+    this._limit *= 2;
+    this._storage = makeLimitedArray(this._limit);
+    var self = this;
+    tempArr.forEach(function(el, index, col) {
+      if (el !== undefined) {
+        for (j = 0; j < el.length; j++) {
+          if (el[j] !== undefined) {
+            var i = getIndexBelowMaxForKey(el[j].k, self._limit);
+            self.insert(i, el[j].v);
+          }
+        }
+      }
+    });
+  }
+}
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var arr = this._storage.get(i);
+
   if (arr === undefined) {
     var value = [];
     value.push({k: k, v: v});
     this._storage.set(i, value);
+    this._size++;
   } else {
     arr.push({k:k, v:v});
     this._storage.set(i, arr);
   }
+  this.resize();
 };
 
 HashTable.prototype.retrieve = function(k){
@@ -23,7 +52,8 @@ HashTable.prototype.retrieve = function(k){
     if (arr[j].k === k) {
       return arr[j].v;
     }
-  }
+  } // else
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
@@ -31,10 +61,14 @@ HashTable.prototype.remove = function(k){
   var arr = this._storage.get(i);
   for (var j = 0; j < arr.length; j++) {
     if (arr[j].k === k) {
-      arr[j] = {k: k, v: null};
+      arr.splice(j, 1);
     }
   }
+  if (arr.length === 0) {
+    this._size--;
+  }
   this._storage.set(i, arr);
+  this.resize();
 };
 
 
